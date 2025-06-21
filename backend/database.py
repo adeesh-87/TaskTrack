21661ba81ee235dc450b00ef185545a208780a61
Adeesh <adeesh.kale@gmail.com>
2025-06-21 14:29:43 +0530

    Added gitignore and better mysql security

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Reads the .env file
+
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 # DATABASE_URL = "sqlite:///./taskmark.db"
-DATABASE_URL = "mysql+pymysql://u421163761_testuser:testPass111!!!!!!!@srv1871.hstgr.io:3306/u421163761_tasktrack"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
